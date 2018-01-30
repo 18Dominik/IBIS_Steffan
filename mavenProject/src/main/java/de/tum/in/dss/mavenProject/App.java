68db66075441169a94262f19a27305860c224fa2
Steffan <ga48yur@mytum.de>
2018-01-30 15:56:34 +0100

    Embedded GurobiDependeny in MavenEclipseProject

---
+++ mavenProject/src/main/java/de/tum/in/dss/mavenProject/App.java
@@ -0,0 +1,13 @@
+package de.tum.in.dss.mavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
